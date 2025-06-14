bb4bde262464a4b323dad7632f9a187b08aadc63
Konrad Grzanek <kongra@gmail.com>
2025-06-14 15:23:09 +0200

    The initial project structure configured

---
+++ cmd/gopro-1/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/kongra/gopro-1/internal/routes"
+)
+
+func main() {
+	const x int = routes.SomeValue
+	fmt.Println("Teściorek", x)
+}
---
+++ internal/routes/routes.go
@@ -0,0 +1,5 @@
+package routes
+
+const (
+	SomeValue = 627
+)
